0972563907462fa5481f3cb881e888ecd6cca07c
Gabrielasosa <gabrielasosaft@gmail.com>
2018-06-27 00:01:22 +0200

    arreglando pruena

--- public/js/min.js
+++ public/js/min.js
@@ -1,108 +1,99 @@
 $(document).ready(function () {
-var guardar = $('#guardar');
-var pintar =$('#pintar')
+    var guardar1 = $('#guardar_cocinero');
+    var guardar2 = $('#guardar_usuario')
+  
 
-// validador
-$.validate({
-    lang: 'es',
-    modules : 'security'
+    // validador
+    $.validate({
+        lang: 'es',
+        modules: 'security'
 
-});
-//--------obtener fecha actual
-var d = new Date();
+    });
+    //--------obtener fecha actual
+    var d = new Date();
 
-var month = d.getMonth()+1;
-var day = d.getDate();
+    var month = d.getMonth() + 1;
+    var day = d.getDate();
 
-var output = d.getFullYear() + '/' +
-    (month<10 ? '0' : '') + month + '/' +
-    (day<10 ? '0' : '') + day;
-//---------------
+    var output = d.getFullYear() + '/' +
+        (month < 10 ? '0' : '') + month + '/' +
+        (day < 10 ? '0' : '') + day;
+    //---------------
 
 
-//Registro de Usuarios
-guardar.on('click',function(){
-    let nombre = $('#nombre').val();
-    let apellido = $('#apellido').val();
-    let email = $('#email').val();
-    let ciudad = $('#ciudad').val();
-    let provincia = $('#provincia').val();
-    let password = $('#contrasenia').val();
-    let telefono = $('#telefono').val();
-    let fecha = output;
+    //Registro de Usuarios
+    guardar1.on('click', function () {
+        let nombre = $('#nombre').val();
+        let apellido = $('#apellido').val();
+        let email = $('#email').val();
+        let ciudad = $('#ciudad').val();
+        let provincia = $('#provincia').val();
+        let password = $('#contrasenia').val();
+        let telefono = $('#telefono').val();
+        let fecha = output;
 
-$.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellido , Email:email , Password:password , Provincia:provincia , Ciudad:ciudad , Telefono:telefono , FechaInicio:fecha , idRol:"U"}, function(showchef){
-    
-} );//fin post
+        $.post('http://localhost:3000/showchef/add', {
+            Nombre: nombre,
+            Apellido: apellido,
+            Email: email,
+            Password: password,
+            Provincia: provincia,
+            Ciudad: ciudad,
+            Telefono: telefono,
+            FechaInicio: fecha,
+            idRol: "U"
+        }, function (showchef) {
 
-} )//fin funcion
+        }); //fin post
 
-//-------------------Registro de Chefs
-// guardar.on('click',function(){
-//     let nombre = $('#nombre').val();
-//     let apellido = $('#apellido').val();
-//     let email = $('#email').val();
-//     let ciudad = $('#ciudad').val();
-//     let provincia = $('#provincia').val();
-//     let password = $('#contrasenia').val();
-//     let telefono = $('#telefono').val();
-//     let fecha = output;
+    }) //fin funcion
 
-// $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellido , Email:email , Password:password , Provincia:provincia , Ciudad:ciudad , Telefono:telefono , FechaInicio:fecha , idRol:"C"}, function(showchef){
-    
-// } );//fin post
-// } )//fin funcion
+    //-------------------Registro de Chefs
+    guardar2.on('click',function(){
+        let nombre = $('#nombre').val();
+        let apellido = $('#apellido').val();
+        let email = $('#email').val();
+        let ciudad = $('#ciudad').val();
+        let provincia = $('#provincia').val();
+        let password = $('#contrasenia').val();
+        let telefono = $('#telefono').val();
+        let fecha = output;
 
-//------------------Guardar segun su rol
-// function on(){
-//     guardar.on('click',function(){
-//         let nombre = $('#nombre').val();
-//         let apellido = $('#apellido').val();
-//         let email = $('#email').val();
-//         let ciudad = $('#ciudad').val();
-//         let provincia = $('#provincia').val();
-//         let password = $('#contrasenia').val();
-//         let telefono = $('#telefono').val();
-//         let fecha = output;
-    
-//     $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellido , Email:email , Password:password , Provincia:provincia , Ciudad:ciudad , Telefono:telefono , FechaInicio:fecha , idRol:"U"}, function(showchef){
+    $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellido , Email:email , Password:password , Provincia:provincia , Ciudad:ciudad , Telefono:telefono , FechaInicio:fecha , idRol:"C"}, function(showchef){
+
+    } );//fin post
+    } )//fin funcion
+
+    //------------------Guardar segun su rol
+    // function on(){
+    //     guardar.on('click',function(){
+    //         let nombre = $('#nombre').val();
+    //         let apellido = $('#apellido').val();
+    //         let email = $('#email').val();
+    //         let ciudad = $('#ciudad').val();
+    //         let provincia = $('#provincia').val();
+    //         let password = $('#contrasenia').val();
+    //         let telefono = $('#telefono').val();
+    //         let fecha = output;
+
+    //     $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellido , Email:email , Password:password , Provincia:provincia , Ciudad:ciudad , Telefono:telefono , FechaInicio:fecha , idRol:"U"}, function(showchef){
+
+    //     } );//fin post
+
+    //     } )//fin funcion
+    //   }
+
+
+    //-------------------ocultar y reaparecer texto
+$(function(){
+    $("#btn_cocinero").click(function(){
+        $("#cont_cocinero").toggle();
+
+    });
+    $("#btn_usuario").click(function(){
+        $("#cont_usuario").toggle();
         
-//     } );//fin post
-    
-//     } )//fin funcion
-//   }
-  
-//   function off(){
-//     console.log("Hemos pulsado en off");
-//   }
-  
-//   var checkbox = document.getElementById('1');
-  
-//   checkbox.addEventListener("change", comprueba, false);
-  
-//   function comprueba(){
-//     if(checkbox.checked){
-//         on();
-//     }else{
-//        off();
-//     }
-//   }
+    });
+})
 
-  
-
-
-
-
-
-// })
-
-//-------------------
-function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find(".more-less")
-        .toggleClass('glyphicon-plus glyphicon-minus');
-}
-$('.panel-group').on('hidden.bs.collapse', toggleIcon);
-$('.panel-group').on('shown.bs.collapse', toggleIcon);
-});//fin document.ready
+}); //fin document.ready
