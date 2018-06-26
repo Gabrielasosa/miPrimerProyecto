7bc806715d688dc0cdc36e196ac14ab8d38867e9
Gabriela <gabrielasosaft@gmail.com>
2018-06-26 14:10:10 +0200

    noseeeee

--- public/js/min.js
+++ public/js/min.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
 var guardar = $('#guardar');
 var pintar =$('#pintar')
 
+// validador
+$.validate({
+    lang: 'es',
+    modules : 'security'
+
+});
 //--------obtener fecha actual
 var d = new Date();
 
@@ -28,9 +34,10 @@ guardar.on('click',function(){
 $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellido , Email:email , Password:password , Provincia:provincia , Ciudad:ciudad , Telefono:telefono , FechaInicio:fecha , idRol:"U"}, function(showchef){
     
 } );//fin post
+
 } )//fin funcion
 
-//Registro de Chefs
+//-------------------Registro de Chefs
 // guardar.on('click',function(){
 //     let nombre = $('#nombre').val();
 //     let apellido = $('#apellido').val();
@@ -45,17 +52,53 @@ $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellid
     
 // } );//fin post
 // } )//fin funcion
-guardar.on('click',function() {
-        $("form").remove()
-        $('#pintar').after("<p>El Usuario se creo correctamente</p>")
-;
-})
+
+//------------------Guardar segun su rol
+// function on(){
+//     guardar.on('click',function(){
+//         let nombre = $('#nombre').val();
+//         let apellido = $('#apellido').val();
+//         let email = $('#email').val();
+//         let ciudad = $('#ciudad').val();
+//         let provincia = $('#provincia').val();
+//         let password = $('#contrasenia').val();
+//         let telefono = $('#telefono').val();
+//         let fecha = output;
+    
+//     $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellido , Email:email , Password:password , Provincia:provincia , Ciudad:ciudad , Telefono:telefono , FechaInicio:fecha , idRol:"U"}, function(showchef){
+        
+//     } );//fin post
+    
+//     } )//fin funcion
+//   }
+  
+//   function off(){
+//     console.log("Hemos pulsado en off");
+//   }
+  
+//   var checkbox = document.getElementById('1');
+  
+//   checkbox.addEventListener("change", comprueba, false);
+  
+//   function comprueba(){
+//     if(checkbox.checked){
+//         on();
+//     }else{
+//        off();
+//     }
+//   }
+
+  
 
 
-// validador
-$.validate({
-    lang: 'es',
-    modules : 'security'
 
-});
+
+
+// })
+
+
+
+
+
+
 });//fin document.ready
