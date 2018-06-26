28f1270b5a5056c574a9fae74a2fb8d04d6e77da
Gabriela <gabrielasosaft@gmail.com>
2018-06-26 17:28:13 +0200

    cambios

--- public/js/min.js
+++ public/js/min.js
@@ -96,9 +96,13 @@ $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellid
 
 // })
 
-
-
-
-
-
+//-------------------
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
 });//fin document.ready
