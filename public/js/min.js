008ff1eb0903731a2e6e51f54ffb7f50fa996a44
Gabriela <gabrielasosaft@gmail.com>
2018-06-27 13:04:57 +0200

    la insercion de datos en cocinero fnciona

--- public/js/min.js
+++ public/js/min.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var guardar1 = $('#guardar_cocinero');
     var guardar2 = $('#guardar_usuario')
-  
+
 
     // validador
     $.validate({
@@ -21,48 +21,52 @@ $(document).ready(function () {
     //---------------
 
 
-    //Registro de Usuarios
-    guardar1.on('click', function () {
-        let nombre = $('#nombre').val();
-        let apellido = $('#apellido').val();
-        let email = $('#email').val();
-        let ciudad = $('#ciudad').val();
-        let provincia = $('#provincia').val();
-        let password = $('#contrasenia').val();
-        let telefono = $('#telefono').val();
-        let fecha = output;
+  //Registro de Usuarios
+  guardar2.on('click', function () {
+    let nombre = $('#nombre').val();
+    let apellido = $('#apellido').val();
+    let email = $('#email').val();
+    let ciudad = $('#ciudad').val();
+    let provincia = $('#provincia').val();
+    let password = $('#contrasenia').val();
+    let telefono = $('#telefono').val();
+    let fecha = output;
 
-        $.post('http://localhost:3000/showchef/add', {
-            Nombre: nombre,
-            Apellido: apellido,
-            Email: email,
-            Password: password,
-            Provincia: provincia,
-            Ciudad: ciudad,
-            Telefono: telefono,
-            FechaInicio: fecha,
-            idRol: "U"
+    $.post('http://localhost:3000/showchef/add', {
+        Nombre: nombre,
+        Apellido: apellido,
+        Email: email,
+        Password: password,
+        Provincia: provincia,
+        Ciudad: ciudad,
+        Telefono: telefono,
+        FechaInicio: fecha,
+        idRol: "U"
+    }, function (showchef) {
+
+    }); //fin post
+
+}) //fin funcion
+
+    //-------------------Registro de Chefs------------------
+    guardar1.on('click', function () {
+        let Nombre_c = $('#nombrec').val();
+        let Apellido_c = $('#apellidoc').val();
+        let Email_c = $('#emailc').val();
+        let Ciudad_c = $('#ciudadc').val();
+        let Provincia_c = $('#provinciac').val();
+        let Password_c = $('#contraseniac').val();
+        let Telefono_c = $('#telefonoc').val();
+        let FechaInicio_c = output;
+        let Especialidad_c = $('#especialidad').val();
+
+        $.post('http://localhost:3000/showchef_cocinero/add', {
+            Nombre_c: Nombre_c, Apellido_c: Apellido_c, Email_c: Email_c, Password_c: Password_c, Provincia_c: Provincia_c,
+            Ciudad_c: Ciudad_c, Telefono_c: Telefono_c, FechaInicio_c: FechaInicio_c, idRol_c: "C",Especialidad_c:Especialidad_c
         }, function (showchef) {
 
-        }); //fin post
-
-    }) //fin funcion
-
-    //-------------------Registro de Chefs
-    guardar2.on('click',function(){
-        let nombre = $('#nombre').val();
-        let apellido = $('#apellido').val();
-        let email = $('#email').val();
-        let ciudad = $('#ciudad').val();
-        let provincia = $('#provincia').val();
-        let password = $('#contrasenia').val();
-        let telefono = $('#telefono').val();
-        let fecha = output;
-
-    $.post('http://localhost:3000/showchef/add', { Nombre: nombre, Apellido: apellido , Email:email , Password:password , Provincia:provincia , Ciudad:ciudad , Telefono:telefono , FechaInicio:fecha , idRol:"C"}, function(showchef){
-
-    } );//fin post
-    } )//fin funcion
+        });//fin post
+    })//fin funcion
 
     //------------------Guardar segun su rol
     // function on(){
@@ -85,15 +89,23 @@ $(document).ready(function () {
 
 
     //-------------------ocultar y reaparecer texto
-$(function(){
-    $("#btn_cocinero").click(function(){
-        $("#cont_cocinero").toggle();
 
-    });
-    $("#btn_usuario").click(function(){
-        $("#cont_usuario").toggle();
-        
-    });
-})
+    $("#cont_usuario").hide()
+    $("#cont_cocinero").hide()
+    $(function () {
+        $("#btn_cocinero").click(function () {
+            $("#cont_usuario").hide();
+            $("#cont_cocinero").show()
+
+
+        });
+        $("#btn_usuario").click(function () {
+            $("#cont_cocinero").hide()
+            $("#cont_usuario").show();
+
+
+        });
+    })
+
 
 }); //fin document.ready
--- routes.js
+++ routes.js
@@ -10,12 +10,15 @@ app.get('/index', function (req, res) {
     res.render('index');
 })
 
-app.get('/chef', function (req, res) {
-    res.render('chef');
+app.get('/pruebas', function (req, res) {
+    res.render('pruebas');
 })
+
+//----------------todas las rutas para usuario-----------------
+
 //ruta para añadir usuarios
 
-app.post('/showchef/add', function (req, res) {
+app.post('/showchef_user/add', function (req, res) {
     //req.body es el cuerpo de la peticion
     let sql = `INSERT INTO usuario (Nombre, Apellido,Email,Provincia,Ciudad,Telefono,FechaInicio,idRol,Password) 
     VALUES ('${req.body.Nombre}','${req.body.Apellido}','${req.body.Email}','${req.body.Provincia}'
@@ -26,7 +29,7 @@ app.post('/showchef/add', function (req, res) {
             res.send(err);
         }
         else {
-            let showchef = {
+            let showchef_user = {
                 IdUsuario: result.IdUsuario,
                 Nombre: req.body.Nombre,
                 Apellido: req.body.Apellido,
@@ -39,14 +42,13 @@ app.post('/showchef/add', function (req, res) {
                 Password: req.body.Password
 
             }
-            res.send(showchef);
+            res.send(showchef_user);
         }
     });
 
 
 
 });
-
 //post original
 /* app.post('/showchef/add', function (req, res) {
     //req.body es el cuerpo de la peticion
@@ -121,4 +123,41 @@ app.post('/showchef/update', function (req, res) {
             // }
         }
     });
+});
+
+//----------------todas las rutas para Cocinero-----------------
+
+//ruta para añadir Cocinero
+
+app.post('/showchef_cocinero/add', function (req, res) {
+    //req.body es el cuerpo de la peticion
+    let sql = `INSERT INTO cocinero (Nombre_c, Apellido_c,Email_c,Provincia_c,Ciudad_c,Telefono_c,FechaInicio_c,idRol_c,Password_c,Especialidad_c) 
+    VALUES ('${req.body.Nombre_c}','${req.body.Apellido_c}','${req.body.Email_c}','${req.body.Provincia_c}'
+    ,'${req.body.Ciudad_c}','${req.body.Telefono_c}','${req.body.FechaInicio_c}','${req.body.idRol_c}','${req.body.Password_c}','${req.body.Especialidad_c}')`;
+    console.log(sql)
+    con.query(sql, function (err, result) {
+        if (err) {
+            console.log(err)
+            res.send(err);
+        }
+        else {
+            let showchef_cocinero = {
+                idCocinero: result.idCocinero,
+                Nombre: req.body.Nombre_c,
+                Apellido: req.body.Apellido_c,
+                Email: req.body.Email_c,
+                Provincia: req.body.Provincia_c,
+                Ciudad: req.body.Ciudad_c,
+                Telefono: req.body.Telefono_c,
+                FechaInicio: req.body.FechaInicio_c,
+                idRol: req.body.idRol_c,
+                Password: req.body.Password_c,
+                Especialidad_c:req.body.Especialidad_c
+
+            }
+            res.send(showchef_cocinero );
+        }
+    });
+
+
 });
